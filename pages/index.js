a86bc6eb0daffef88839969c60a7dad8c11dfbfa
AndrewJBateman <gomezbateman@yahoo.com>
2022-03-21 10:35:28 +0100

    chore: dependencies installed, linting

--- pages/index.js
+++ pages/index.js
@@ -1,30 +1,48 @@
 import Head from "next/head";
-import Image from "next/image";
+import NextImage from "next/image";
 import styles from "../styles/Home.module.css";
 
-export default function Home() {
-	return (
-		<div>
-			<section className="container mx-auto p-10 md:p-20 transform duration-500">
-				<article className="shadow-lg mx-auto relative max-w-sm">
-					<img
-						className="w-full h-auto"
-						src="https://weandthecolor.com/wp-content/uploads/2012/03/Colorphobia-Illustration-by-Matheus-Lopes-34645646.jpg"
-						alt=""
-					/>
-					<span className="absolute right-4 top-4 bg-gray-600 text-white rounded-md p-1 pl-4 pr-4 font-semibold">
-						The Wizard
-					</span>
-					<div className="p-7 my-auto pb-10 ">
-						<h1 className="text-2xl font-semibold text-gray-800">
-							Masterpiece
-						</h1>
-						<p className="text-base text-gray-400 mt-2">
-							Random text
-						</p>
-					</div>
-				</article>
-			</section>
-		</div>
-	);
-}
+const Home = () => {
+  return (
+    <>
+      <Head>
+        <title>Home page</title>
+      </Head>
+      <section className="container p-10 mx-auto duration-500 transform md:p-20">
+        <article className="relative max-w-sm mx-auto shadow-lg">
+          <NextImage
+            className=""
+            width="400"
+            height="300"
+            src="https://source.unsplash.com/random/400x300"
+            alt="random photo"
+          />
+          <span className="absolute p-1 pl-4 pr-4 font-semibold text-white bg-gray-600 rounded-md right-4 top-4">
+            Unsplash Random Image
+          </span>
+          <div className="pb-10 my-auto p-7 ">
+            <h1 className="text-2xl font-semibold text-gray-800">
+              Quote of the Day
+            </h1>
+            <p className="mt-2 text-base text-gray-400"></p>
+          </div>
+        </article>
+      </section>
+    </>
+  );
+};
+
+// Home.getInitialProps = async function() {
+//   const res = await fetch('https://quotes.rest/qod');
+//   const data = await res.json();
+//   quoteData = data.contents.quotes;
+//   console.log('data:', quoteData);
+
+//   return {
+//     props: {
+//       quoteData
+//     }
+//   }
+// }
+
+export default Home;
